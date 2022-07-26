4bae654bceeacb3c4746b71c5222a2f06b682120
Kyle Tillson <kyletillson004@gmail.com>
2022-07-26 16:23:12 -0600

    work

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
+
 document.addEventListener("DOMContentLoaded", function() {});
+
+fetch(`http://localhost:3000/books`)
+.then(res => res.json())
+.then((data) => {
+
+    data.forEach(book => {
+        // create a new li
+        const li = document.createElement('li')
+        //set text of that li to the book.title
+        li.innerText = book.title
+        //append the li to the ul
+        const ul = document.querySelector('#list')
+       
+        
+        ul.append(li)
+
+        
+         })
+         
+    });
+
+
+
+   
+
+
+   
+
+
+
+    
+        
+
+
+
+
+
+        
+
+
